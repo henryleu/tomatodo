05db57d3be1bdccd633ba8c75cbb8c5cbad68573
henryleu <henryleu@126.com>
2013-06-13 18:56:39 +0800

    comment out some redis test code

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,20 @@ var redis = store.redis;
 var mongodb = store.mongodb;
 
 module.exports = function(app) {
+    var checkUserToken = function(req, res, next) {
+        var userToken = req.cookies.userToken;
+        if(userToken){
+            logger.info(userToken);
+        }
+        else{
+            userToken = new Date().getTime();
+            res.cookie('userToken', userToken);
+        }
+    };
+    //app.all('*', checkUserToken);
     app.get('/', function(req, res) {
+        checkUserToken(req, res);
+/*
         redis.set("test", "Hello World", function (err, reply) {
             if(err){
                 logger.error(err);
@@ -18,6 +31,7 @@ module.exports = function(app) {
             logger.debug(reply);
         });
         req.session.user = {name: 'henryleu', signinStatus: true};
+*/
         res.render('index',
             {
                 displayName: '你'
